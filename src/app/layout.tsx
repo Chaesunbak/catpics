89dc15284c348515d8c79080060246a6cc940f80
Chaesunbak <coanstjd0422@gmail.com>
2024-08-21 17:57:29 +0900

    feat : add cool favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
-import cn from "clsx";
 
 const work_sans = Work_Sans({
   subsets: ["latin"],
@@ -11,6 +10,9 @@ const work_sans = Work_Sans({
 export const metadata: Metadata = {
   title: "Many Cat Pcis",
   description: "come here to see many cat pics",
+  icons: {
+		icon: "/favicon.ico",
+	},
 };
 
 export default function RootLayout({
